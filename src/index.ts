446c836e3a630eda02ba55ac2bfdf6a3cdf86882
2halgas <khamidulla.2halgas@gmail.com>
2025-03-09 23:40:59 +0500

    feat: add logs

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ import authRoutes from "./routes/authRoutes";
 import rateLimit from "express-rate-limit";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+import { RotatingFileStream, createStream } from "rotating-file-stream";
 
 dotenv.config();
 
@@ -19,6 +23,29 @@ const port = process.env.PORT || 3000;
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "10kb" }));
+
+// Console logging (dev mode)
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// File logging
+const logDirectory = path.join(__dirname, "logs");
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+const accessLogStream = createStream("access.log", {
+  interval: "1d", // Rotate daily
+  path: logDirectory,
+  compress: "gzip", // Compress old logs
+});
+
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+    { stream: accessLogStream }
+  )
+);
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
